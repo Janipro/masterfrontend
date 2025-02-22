de445f2aad1b0c8f393fcfaf7851ccf9d9ea2566
Jan Adrian Hafskjold <jahafskj@stud.ntnu.no>
2025-02-22 09:17:11 +0100

    Update stat page

--- src/pages/Statistics.tsx
+++ src/pages/Statistics.tsx
@@ -1,15 +1,14 @@
 import { Box } from '@mui/material';
 import NavBar from '../components/NavBar';
 import useTeacherStore from '../stores/useTeacherStore';
-import StudentClass from '../components/StudentClass';
-import TeacherClass from '../components/TeacherClass';
+import StudentStatistics from '../components/StudentStatistics';
 
 export default function Class() {
   const { isTeacher } = useTeacherStore();
   return (
     <Box>
       <NavBar isEditor={false} />
-      {isTeacher ? <TeacherClass /> : <StudentClass />}
+      {isTeacher ? <StudentStatistics /> : <StudentStatistics />}
     </Box>
   );
 }
---
+++ src/types/typeColors.ts
@@ -0,0 +1,8 @@
+import { blue, green, orange, red } from '@mui/material/colors';
+
+export const TYPE_COLORS = {
+  'if-statement': red[400],
+  'for-loop': orange[400],
+  'while-loop': green[400],
+  inheritance: blue[400],
+};
